281ae5f77c766ac7865020bc19e2c0423b0761aa
박지인 <jiinbag057@gmail.com>
2022-07-23 08:30:56 +0000

    static url 변경

--- meta4music/settings.py
+++ meta4music/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(*^^ajyl3t%=r!8p65on*y_=7nxo_m)j3m=5w75yo8r4nqol#^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.goorm.io']
 
 
 # Application definition
@@ -116,11 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # STATIC_ROOT = [os.path.join(BASE_DIR,'static')]
 
