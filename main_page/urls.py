d9f26a674d207a9d1e9bb5eca26fac55cda0787d
Jeein Park <96341808+jeein2222@users.noreply.github.com>
2022-08-05 19:42:14 +0900

    post url 추가

--- main_page/urls.py
+++ main_page/urls.py
@@ -9,5 +9,7 @@ urlpatterns=[
     path('home',views.home_view, name="home"),
     path('drawing',views.drawing_view, name="drawing"),
     path('playing',views.playing_view, name="playing"),
+    path('post',views.post, name="post"),
+    path('playing/<str:lyric>/',views.makeLyric, name="makeLyric") 
 
 ]
