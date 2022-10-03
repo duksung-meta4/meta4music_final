00cd4661d20087a59feda71afa342d13b40c01d9
jeein2222 <jiinbag057@gmail.com>
2022-10-03 17:09:44 +0900

    로그인 상태 유지

--- main_page/urls.py
+++ main_page/urls.py
@@ -7,10 +7,11 @@ app_name='main_page';
 
 urlpatterns=[
     path('', views.home, name="main"),
-    path('home',views.home_view, name="home"),
+    path('home',views.home, name="home"),
     path('drawing',views.drawing_view, name="drawing"),
     path('playing',views.playing_view, name="playing"),
     path('post',views.post, name="post"),
+    path('result',views.result,name="result"),
     path('playing/<str:lyric>/',views.makeLyric, name="makeLyric"),
     path('composing/',views.compose, name="compose"),
     path('composing/<str:keyword>/',views.composing, name="composing"), 
