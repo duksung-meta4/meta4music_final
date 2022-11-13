b67186303e5d178a1cd5dd9c50a284daba3d8782
Hyojeong Shin <kyn05298@naver.com>
2022-11-13 18:46:03 +0900

    static 파일들을 하나의 경로로 setting

--- meta4music/settings.py
+++ meta4music/settings.py
@@ -186,6 +186,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'main_page\\static'),os.path.join(BAS
 
 
 # STATIC_ROOT = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
